3149d7f8e877aed5de6625e30cc78977558d0d19
evanbangun <evan.bangun.eb@gmail.com>
2016-12-19 15:10:14 +0700

    tes

---
+++ lobby.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((600, 400))
+
+# pygame.display.update()
+
+gameExit = False
+
+font = pygame.font.SysFont(None,25)
+
+def message_to_screen(msg,color):
+	screen_text = font.render(msg,True,color)
+	gameDisplay.blit(screen_text,[450,350])
+
+def main():
+    pygame.display.set_mode((600, 400))
+    pygame.display.set_caption('Lobby')
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    exit()
+
+        gameDisplay.fill(black)
+        pygame.draw.rect(gameDisplay, (40,40,40), [10,10,580,300])
+        cur = pygame.mouse.get_pos()
+        if cur[0] > 450 and cur[0] < 565 and cur[1] > 350 and cur[1] < 370:
+            message_to_screen("Create Room", red)
+        else:
+            message_to_screen("Create Room", white)
+        pygame.display.update()
+
+    pygame.quit()
+    quit()
